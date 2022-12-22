38082afbfa55a8307edca9ba14d8639eebea2b29
Manuel Botija <mbotija@graimatterlabs.ai>
2022-12-23 00:04:25 +0100

    more tooling, added ability to skip frames

--- training_pipe.py
+++ training_pipe.py
@@ -11,6 +11,7 @@ from pygame import Rect
 from utils import *
 import sys
 import pandas as pd
+import copy
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -54,7 +55,7 @@ def run_training(options, model_class, wrapper_settings, hyperparams=None, model
                         final_plot=False)
     
     durations, rewards, success, loss, average_score, speed = agent.do_train(
-                                                        num_episodes = 3000,
+                                                        num_episodes = 500,
                                                         target_avg_score=100000)
     
 
@@ -69,119 +70,126 @@ def run_training(options, model_class, wrapper_settings, hyperparams=None, model
     return save_path, success, average_score, speed
 
 
-def _init_table():
-    # Create a DataFrame with column names specified
-    table = pd.DataFrame(columns=['save_path','gamma', 'eps_decay', 'eps_end', 'batch_size', 'reward_type', 'average_score', 'speed', 'success'])
+def _init_table(hyperParams, options, wrapper_settings):
+    """creates a table with all the dict keys of input arguments as columns"""
+    table = pd.DataFrame(columns=[])
+    table['save_path'] = None
+    for key in hyperParams.__dict__.keys():
+        table[key] = None
+    for key in options.__dict__.keys():
+        table[key] = None
+    for key in wrapper_settings.__dict__.keys():
+        table[key] = None 
+    table['avg_score'] = None
+    table['avg_speed'] = None
+    table['success'] = None
     return table
 
-def _add_to_table(table, hyperParams, average_score, speed, save_path, reward_type, success):
-    # Create a new dataframe with the new row
+def _add_to_table(table, hyperParams, options, wrapper_settings, average_score, speed, save_path, success):
+    # Create a new row for the table
+    # it adds the values of the hyperparameters, options and warappers to the new row
     success = bool(success)
-    new_row = pd.DataFrame({'save_path': save_path,
-                        'gamma': hyperParams.gamma,
-                        'eps_decay': hyperParams.eps_decay,
-                        'eps_end': hyperParams.eps_end,
-                        'batch_size': hyperParams.batch_size,
-                        'reward_type': reward_type,
-                        'average_score': average_score,
-                        'speed': speed, 
-                        'success': success}, index=[0])
-    # Concatenate the new row to the existing dataframe
+    new_row = pd.DataFrame(columns=[], index=[0])
+    new_row['save_path'] = save_path
+    for key in hyperParams.__dict__.keys():
+        new_row[key] = hyperParams.__dict__[key]
+    for key in options.__dict__.keys():
+        new_row[key] = options.__dict__[key]
+    for key in wrapper_settings.__dict__.keys():
+        new_row[key] = wrapper_settings.__dict__[key]
+    new_row['avg_score'] = average_score
+    new_row['avg_speed'] = speed
+    new_row['success'] = success
     table = pd.concat([table, new_row], ignore_index=True)
-    return table
+    return table 
+
+def print_reduced_table(table):
+    """removes the columns that have the same value for all rows"""
+    reduced_table = table.copy()
+    for col in table.columns:
+        if len(table[col].unique()) == 1:
+            reduced_table = reduced_table.drop(col, axis=1)
+    print(reduced_table)
 
 
 def hyperparameter_search(options, hyperParams, wrapper_settings):
-# Hyperparameter search
-    gamma_list = [0.98]
-    eps_decay_list = [4000]
-    eps_end_list = [0.05]
-    batch_size_list = [128]
-    reward_type_list = [4]
 
     max_tries_per_config = 1
+    table = _init_table(hyperParams, options, wrapper_settings)
 
-    #iterate over all configs
-    best_average_score = 0
-    best_speed = sys.maxsize
-    best_hyperparams = None
-    best_options = None
-    table = _init_table()
+    for option in get_next_hyperparameter(options):
+        for hyper_param in get_next_hyperparameter(hyperParams):
+            for wrapper_setting in get_next_hyperparameter(wrapper_settings):
+                for i in range(max_tries_per_config):
+                    save_path, success, average_score, avg_speed = run_training(
+                                                                option,
+                                                                SimpleLinearModel,
+                                                                wrapper_setting, 
+                                                                hyper_param)
 
-    for batch_size in batch_size_list:
-        for gamma in gamma_list:
-            for eps_decay in eps_decay_list:
-                for eps_end in eps_end_list:
-                    for reward_type in reward_type_list:
-                        hyperParams.gamma = gamma
-                        hyperParams.eps_decay = eps_decay
-                        hyperParams.eps_end = eps_end
-                        hyperParams.batch_size = batch_size
-                        options.reward_type = reward_type
-                        for i in range(max_tries_per_config):
-                            save_path, success, average_score, avg_speed = run_training(
-                                                                        options,
-                                                                        SimpleLinearModel,
-                                                                        wrapper_settings, 
-                                                                        hyperParams)
+                    table = _add_to_table(table, hyper_param, option, wrapper_setting, average_score, avg_speed, save_path, success)
+                    print(table)
 
-                            table = _add_to_table(table, hyperParams, average_score, avg_speed, save_path, reward_type, success)
-                            print(table)
-
-                            if average_score > best_average_score:
-                                best_average_score = average_score
-                                best_speed = avg_speed
-                                best_hyperparams = hyperParams
-                                best_options = options
-                                best_reward_type = reward_type
-                                print("=====New best score ", average_score, "with speed", avg_speed, "======")
-                            if success: 
-                                print("=====Success! ======")
-                                print(table)
-                                return
+                    if success: 
+                        print("=====Success! ======")
+                        print_reduced_table(table)
+                        return
 
     print("==============================================")
-    print("Failed to solve task with any config")
-    print("Best config: ", best_hyperparams)
-    print("with options: ", options)
-    print("with reward_type: ", best_reward_type)
-    print("resulted in average speed: ",best_speed, " and average_score: ", best_average_score, "======")
-    print("Table of results: ")
-    print(table)
+    print("Search complete. Result summary: ")
+    print_reduced_table(table)
     print("==============================================")
     return
 
+def get_next_hyperparameter(input_set):
+    """
+    Method that automatically iterates over the input. 
+    The input is expected to be instances of a class that containes the parameters to be iterated over.
+    Whenever it finds a parameter that is a list it creates
+    as many copies of the input set as there are elements in the list
+    and replaces the parameter with the corresponding element in the list.
+    It yields the new input set for each iteration.
+    """
+    output_set = [input_set]
+    for_removal = []
+    for set in output_set:
+        for key in set.__dict__:
+            value = set.__dict__[key]
+            if isinstance(value, list):
+                for i in range(len(value)):
+                    new_set = copy.deepcopy(set)
+                    new_set.__dict__[key] = value[i]
+                    output_set.append(new_set)
+                for_removal.append(set)
+                break
+
+    for set in for_removal:
+        output_set.remove(set)
+    
+    print("Number of configs found for ", input_set.__class__ ,", : ", len(output_set))
+    for set in output_set:
+        yield set
 
 wrapper_settings = TorchWrapperSettings(
-                        normalize=True, 
-                        flatten_action=True)
+                        normalize=[True, False], 
+                        flatten_action=True,
+                        skip_frames=[None, 1,2, 3])
 
 
 hyperParams = HyperParams(
                         memory_size=10000, 
-                        batch_size=None, 
-                        gamma=None, 
+                        batch_size=[128,256], 
+                        gamma=[0.95,0.99], 
                         tau=0.005,
                         eps_start=0.95,
-                        eps_end=None,
-                        eps_decay=None,
+                        eps_end=0.05,
+                        eps_decay=4000,
                         lr=1e-4
                         )
 
-# options = GameOptions(  height=600, 
-#                         width=400, 
-#                         n_obstacles=1, 
-#                         n_turrets=1, 
-#                         max_projectiles_per_turret=3, 
-#                         fire_turret_step_delay=30,
-#                         max_steps=300,
-#                         reward_type=None,
-#                         )
-
-# options.player_bounds = Rect(0, 0, options.width, options.height)
-# options.gate_bounds= Rect(0, 0, options.width, options.height)
-
 options = GameOptions()
-options.max_steps = 300
+options.max_steps = [300,500]
+options.max_projectiles_per_turret = 0
+options.reward_type = [4,5]
 
 hyperparameter_search(options, hyperParams, wrapper_settings)
